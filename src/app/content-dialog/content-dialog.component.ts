38ae2516d6e886929054ef89621262417d47f59f
dezaxata <mirela.patrascu0@gmail.com>
2020-10-28 12:23:07 +0200

    2 files modified

--- src/app/content-dialog/content-dialog.component.ts
+++ src/app/content-dialog/content-dialog.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-content-dialog',
-  templateUrl: './content-dialog.component.html',
-  styleUrls: ['./content-dialog.component.css']
+  selector: "app-content-dialog",
+  templateUrl: "./content-dialog.component.html",
+  styleUrls: ["./content-dialog.component.scss"]
 })
 export class ContentDialogComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
